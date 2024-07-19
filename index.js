5813cc0e399f39de1f2686c87bcb62bedcc43231
marcosdiniz2104 <marcosdiniz2104@gmail.com>
2024-07-19 21:33:06 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,192 @@
+const express = require("express");
+const fs = require("fs");
+const cors = require("cors");
+
+const app = express();
+const port = 3000;
+
+const corsOptions = {
+  origin: "http://localhost:4200",
+  optionsSuccessStatus: 204,
+  methods: "GET, POST, PUT, DELETE",
+};
+
+app.use(cors(corsOptions));
+
+app.use(express.json());
+
+app.get('/imagem/:name', (req, res) => {
+  const { name } = req.params; // Nome do arquivo recebido na URL
+  const imagePath = path.join(__dirname, 'assets/images/products', name); // Caminho para o arquivo de imagem
+
+  res.sendFile(imagePath, (err) => {
+    if (err) {
+      res.status(404).send('Imagem não encontrada');
+    }
+  });
+});
+
+// examplo: localhost:3000/clothes?page=0&perPage=2
+app.get("/clothes", (req, res) => {
+  const page = parseInt(req.query.page) || 0;
+  const perPage = parseInt(req.query.perPage) || 10;
+
+  fs.readFile("db.json", "utf8", (err, data) => {
+    if (err) {
+      console.log(err);
+      res.status(500).send("Internal Server Error");
+      return;
+    }
+
+    const jsonData = JSON.parse(data);
+
+    const start = page * perPage;
+    const end = start + perPage;
+
+    const result = jsonData.items.slice(start, end);
+
+    res.status(200).json({
+      items: result,
+      total: jsonData.items.length,
+      page,
+      perPage,
+      totalPages: Math.ceil(jsonData.items.length / perPage),
+    });
+  });
+});
+
+// examplo: localhost:3000/clothes
+/*
+  body: {
+    "image": "https://your-image-url.com/image.png",
+    "name": "T-shirt",
+    "price": "10",
+    "rating": 4
+  }
+*/
+app.post("/clothes", (req, res) => {
+  const { image, name, price, rating } = req.body;
+
+  fs.readFile("db.json", "utf8", (err, data) => {
+    if (err) {
+      console.log(err);
+      res.status(500).send("Internal Server Error");
+      return;
+    }
+
+    const jsonData = JSON.parse(data);
+
+    const maxId = jsonData.items.reduce(
+      (max, item) => Math.max(max, item.id),
+      0
+    );
+
+    const newItem = {
+      id: maxId + 1,
+      image,
+      name,
+      price,
+      rating,
+    };
+
+    jsonData.items.push(newItem);
+
+    fs.writeFile("db.json", JSON.stringify(jsonData), (err) => {
+      if (err) {
+        console.log(err);
+        res.status(500).send("Internal Server Error");
+        return;
+      }
+
+      res.status(201).json(newItem);
+    });
+  });
+});
+
+// examplo: localhost:3000/clothes/1
+/*
+  body: {
+    "image": "https://your-image-url.com/image.png",
+    "name": "T-shirt",
+    "price": "10",
+    "rating": 4
+  }
+*/
+app.put("/clothes/:id", (req, res) => {
+  const id = parseInt(req.params.id);
+  const { image, name, price, rating } = req.body;
+
+  fs.readFile("db.json", "utf8", (err, data) => {
+    if (err) {
+      console.log(err);
+      res.status(500).send("Internal Server Error");
+      return;
+    }
+
+    const jsonData = JSON.parse(data);
+
+    const index = jsonData.items.findIndex((item) => item.id === id);
+
+    if (index === -1) {
+      res.status(404).send("Not Found");
+      return;
+    }
+
+    jsonData.items[index] = {
+      id,
+      image,
+      name,
+      price,
+      rating,
+    };
+
+    fs.writeFile("db.json", JSON.stringify(jsonData), (err) => {
+      if (err) {
+        console.log(err);
+        res.status(500).send("Internal Server Error");
+        return;
+      }
+
+      res.status(200).json(jsonData.items[index]);
+    });
+  });
+});
+
+// examplo: localhost:3000/clothes/1
+app.delete("/clothes/:id", (req, res) => {
+  const id = parseInt(req.params.id);
+
+  fs.readFile("db.json", "utf8", (err, data) => {
+    if (err) {
+      console.log(err);
+      res.status(500).send("Internal Server Error");
+      return;
+    }
+
+    const jsonData = JSON.parse(data);
+
+    const index = jsonData.items.findIndex((item) => item.id === id);
+
+    if (index === -1) {
+      res.status(404).send("Not Found");
+      return;
+    }
+
+    jsonData.items.splice(index, 1);
+
+    fs.writeFile("db.json", JSON.stringify(jsonData), (err) => {
+      if (err) {
+        console.log(err);
+        res.status(500).send("Internal Server Error");
+        return;
+      }
+
+      res.status(204).send();
+    });
+  });
+});
+
+app.listen(port, () => {
+  console.log(`Server listening at http://localhost:${port}`);
+});
+
